c33d7c6409c84e22e43506031318faa1409eea09
Roman Shvets <pomaxa@gmail.com>
2017-11-09 23:40:57 +0200

    base record test;

---
+++ tests/src/RecordTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: r.shvets
+ * Date: 09/11/2017
+ * Time: 23:30
+ */
+
+namespace Tests\Diag;
+
+use Diag\Record;
+use Diag\Severity;
+use PHPUnit\Framework\TestCase;
+
+class RecordTest extends TestCase
+{
+
+    public function testEmptyConstruction()
+    {
+        $record = new Record();
+
+        self::assertTrue(is_array($record->toArray()));
+        self::assertEquals($record->getSeverity(), Severity::LOG);
+        self::assertEquals(
+            ['message' => '', 'severity' => Severity::LOG, 'eventType' => 'general', 'projectId' => 0, 'version' => 0],
+            $record->toArray()
+        );
+    }
+
+    public function testArrayConstruction()
+    {
+        $preset = [
+            'message' => 'Test Message',
+            'severity' => Severity::CRITICAL,
+            'eventType' => 'new',
+            'projectId' => 42,
+            'version' => 7
+        ];
+
+        $record = new Record($preset);
+
+        self::assertTrue(is_array($record->toArray()));
+        self::assertEquals(
+            $preset,
+            $record->toArray()
+        );
+
+        self::assertTrue($record->getId() === null);
+    }
+}
