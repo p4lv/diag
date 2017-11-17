04beb995c2b322b6b256f87642eeace4c5e9d286
Roman Shvets <pomaxa@gmail.com>
2017-11-17 19:12:14 +0200

    implement stub api

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -3,39 +3,49 @@
 namespace Diag\Controller;
 
 
-use Diag\DiagRecord;
-use Diag\DiagRecordIterator;
-use Diag\DiagResponse;
+use Diag\DataMapper;
+use Diag\LogReader;
+use Diag\Record;
+use Diag\Storage\Clickhouse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class Api
 {
-    public function getList(array $filters): JsonResponse
+    public function getList(Request $request): JsonResponse
     {
 
-        /** @var \Diag\Storage\Clickhouse $clickhouse */
-        $clickhouse = $container->get(\Diag\Storage\Clickhouse::class);
+        $dataReader = new LogReader(new \Diag\Storage\Clickhouse);
 
-        $response = new JsonResponse([]);
+        $data = $dataReader->getLast($request->get('limit', 10));
+        $response = new JsonResponse($data);
         return $response;
     }
 
     public function postRecords(Request $request): JsonResponse
     {
-        $response = new JsonResponse([]);
+        $dataMapper = new DataMapper(new \Diag\Storage\Clickhouse);
+        $dataMapper->store($request->request->all());
+
+        $response = new JsonResponse($record->toArray());
         return $response;
     }
 
     public function postRecord(Request $request): JsonResponse
     {
-        $response = new JsonResponse([]);
+        $record = new Record($request->request->all());
+
+        $dataMapper = new DataMapper(new \Diag\Storage\Clickhouse);
+        $dataMapper->store($record);
+
+        $response = new JsonResponse($record->toArray());
         return $response;
     }
 
     public function getRecord(Request $request): JsonResponse
     {
-        $response = new JsonResponse([]);
+        $dataReader = new LogReader(new \Diag\Storage\Clickhouse);
+        $response = new JsonResponse($dataReader->get($request->get('id')));
         return $response;
     }
 }
