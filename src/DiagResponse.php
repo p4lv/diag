bed917d6cc106e8ba9dca2049686a1c538d09dff
Roman Shvets <pomaxa@gmail.com>
2017-11-18 14:45:54 +0200

    array short syntax

--- src/DiagResponse.php
+++ src/DiagResponse.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DiagResponse extends JsonResponse
 {
-    public function __construct($data = null, int $status = 200, array $headers = array(), bool $json = false)
+    public function __construct($data = null, int $status = 200, array $headers = [], bool $json = false)
     {
         if ($data instanceof DiagRecord) {
             $data = $data->toArray();
